4ffd700cd2ef691b0057e35537b029bd0f913a26
beatrizmie <38322370+beatrizmie@users.noreply.github.com>
2018-09-14 10:00:26 -0300

    bn

--- assets/script_pag3.js
+++ assets/script_pag3.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let db = new DB('https://aula-10-a1ec0.firebaseio.com')
+  let db = new DB('https://aula-10-a1ec0.firebaseio.com/')
 
   let tpName = new TP('template-name')
 
